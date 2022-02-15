db38a460fd021174a49eee2a7f3ddcedc33c8a34
Duy Nguyen <dunguyen@adobe.com>
2022-02-15 11:47:59 +0100

    update page path

--- src/AEM_Const.js
+++ src/AEM_Const.js
@@ -1,2 +1,2 @@
 export const AEM_HOST = 'https://author-p15902-e23962-cmstg.adobeaemcloud.com';
-export const AEM_PAGE_PATH = '/content/editorxpdevelopment/us/en';
+export const AEM_PAGE_PATH = '/content/editorxpdevelopment/duy-sandbox/en';
